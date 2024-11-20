c0efbaa31817cd497a7f38aab0c89c4790c56609
smartstandard <smartcode2414@gmail.com>
2024-11-20 17:09:39 +0900

    feat:readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Django_Telegram_messaging
 This is a messaging application for user verification by sending a message from Django to Telegram.
+
+```
+pip install python-telegram-bot
+```
+you can triger 
+
+```
+ http://127.0.0.1:8000/bot/send-message/?username=<username>&message=<message>
+ ```
--- messaging_bot/bot/urls.py
+++ messaging_bot/bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SendMessageView
+from .views import SendMessageView, IndexView
 
 urlpatterns = [
-    path('send-message/', SendMessageView.as_view(), name='send-message'),
+    path('send-message/', SendMessageView.as_view(), name='send_message'),
+    path('', IndexView.as_view(), name='index'),  # Add this line
 ]
